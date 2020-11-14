a1602c4d6e746534e73b18e2b992467d49d003e6
Atlejo <alejandro.naranjo@ecosystems.solutions>
2020-11-14 09:44:27 -0500

    Initial Commit

---
+++ app-files/data.js
@@ -0,0 +1,406 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sala",
+      "name": "Sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.721576394722554,
+        "pitch": 0,
+        "fov": 1.47229449837205
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8579903589032387,
+          "pitch": 0.07443495026534563,
+          "rotation": 6.283185307179586,
+          "target": "4-balcn"
+        },
+        {
+          "yaw": -1.1254082681092576,
+          "pitch": 0.11857758874550228,
+          "rotation": 0,
+          "target": "1-cocina"
+        },
+        {
+          "yaw": 1.0932021892415413,
+          "pitch": -0.08194643100059196,
+          "rotation": 7.853981633974483,
+          "target": "5-cuarto-til"
+        },
+        {
+          "yaw": 1.082646727495547,
+          "pitch": 0.17969500324770848,
+          "rotation": 7.853981633974483,
+          "target": "6-pequeadero"
+        },
+        {
+          "yaw": -0.24060401293432676,
+          "pitch": -0.03942281195241648,
+          "rotation": 10.995574287564278,
+          "target": "3-alcoba-1"
+        },
+        {
+          "yaw": -0.2500648153958167,
+          "pitch": 0.26966985461212545,
+          "rotation": 13.351768777756625,
+          "target": "2-alcoba-principal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.112945794522483,
+          "pitch": -0.20120598763330122,
+          "title": "Apartamento - 54 mt2",
+          "text": "2 Baños&nbsp;<br>3 alcobas <br>Cuatro útil&nbsp;<div>Parqueadero</div>"
+        },
+        {
+          "yaw": 3.088102634950747,
+          "pitch": 0.2245729116256392,
+          "title": "Info",
+          "text": "Alejandro Naranjo<br>311 706 4404"
+        }
+      ]
+    },
+    {
+      "id": "1-cocina",
+      "name": "Cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.2039284642971513,
+        "pitch": 0.07540451051130326,
+        "fov": 1.47229449837205
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20610113637512661,
+          "pitch": 0.19355517818827295,
+          "rotation": 0,
+          "target": "0-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-alcoba-principal",
+      "name": "Alcoba Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.603066630563684,
+          "pitch": -0.03961066580419015,
+          "rotation": 1.5707963267948966,
+          "target": "3-alcoba-1"
+        },
+        {
+          "yaw": 2.7639020434927364,
+          "pitch": 0.09355737281356724,
+          "rotation": 0,
+          "target": "7-zonas-comunes"
+        },
+        {
+          "yaw": 1.6228681382330405,
+          "pitch": 0.4092581379849296,
+          "rotation": 11.780972450961727,
+          "target": "0-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-alcoba-1",
+      "name": "Alcoba #1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.8402517926547102,
+        "pitch": 0.12041733188082304,
+        "fov": 1.47229449837205
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1163200543079252,
+          "pitch": 0.163023657772154,
+          "rotation": 1.5707963267948966,
+          "target": "0-sala"
+        },
+        {
+          "yaw": -1.1196125458522665,
+          "pitch": -0.12811740073948386,
+          "rotation": 4.71238898038469,
+          "target": "2-alcoba-principal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-balcn",
+      "name": "Balcón",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.4834964636708463,
+        "pitch": -0.0039188379769612425,
+        "fov": 1.47229449837205
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6189323310478692,
+          "pitch": 0.10309796933148441,
+          "rotation": 0,
+          "target": "0-sala"
+        },
+        {
+          "yaw": -2.592549720965252,
+          "pitch": 0.08301603332955665,
+          "rotation": 3.9269908169872414,
+          "target": "7-zonas-comunes"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-cuarto-til",
+      "name": "Cuarto Útil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.025251109997068255,
+          "pitch": 0.2633471627908861,
+          "rotation": 0,
+          "target": "6-pequeadero"
+        },
+        {
+          "yaw": -0.023495384469960356,
+          "pitch": -0.11050198745516582,
+          "rotation": 0,
+          "target": "0-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pequeadero",
+      "name": "Pequeadero",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4546448373596679,
+          "pitch": 0.016949495256470826,
+          "rotation": 0,
+          "target": "0-sala"
+        },
+        {
+          "yaw": -0.44527168816311224,
+          "pitch": 0.3246580712149214,
+          "rotation": 0,
+          "target": "5-cuarto-til"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-zonas-comunes",
+      "name": "Zonas Comunes",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9901496462967714,
+          "pitch": -0.345889284769175,
+          "rotation": 4.71238898038469,
+          "target": "0-sala"
+        },
+        {
+          "yaw": 1.231182085442697,
+          "pitch": 0.7060031388908978,
+          "rotation": 0,
+          "target": "6-pequeadero"
+        },
+        {
+          "yaw": 0.8834400540813014,
+          "pitch": 0.6114233030657346,
+          "rotation": 3.141592653589793,
+          "target": "5-cuarto-til"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "enventa.co 311 706 4404",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
